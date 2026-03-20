ac50627c170fa7f6416d02b09194866e8fd4ef78
Satadeep Dasgupta <satadeep.dasgupta@brainiuminfotech.com>
2026-03-20 19:27:32 +0530

    Add proxy configuration to YouTubeTranscriptApi initialization

--- src/services/transcribe.py
+++ src/services/transcribe.py
@@ -1,9 +1,12 @@
 from youtube_transcript_api import YouTubeTranscriptApi
+from youtube_transcript_api.proxies import GenericProxyConfig
 
 
 class TranscribeService:
     def __init__(self):
-        self.ytt = YouTubeTranscriptApi()
+        self.ytt = YouTubeTranscriptApi(
+            proxy_config=GenericProxyConfig(http_url="http://47.243.181.85:41714")
+        )
 
     def transcribe(self, video_id: str):
         transcript = self.ytt.fetch(video_id)
